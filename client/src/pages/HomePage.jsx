c4fdc17881e1210e4d205af09b944ee188b85a71
Exter-dg <prkandmap23@rocketmail.com>
2023-03-27 19:38:00 +0530

    Fix: Folder upload UI

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
-import { Button, Box, Grid, Typography } from "@mui/material";
+import { Button, Box, Grid, Typography, IconButton } from "@mui/material";
 import axios from "axios";
 import { useState } from "react";
 import Lottie from "lottie-react";
 import loadingJson from "../assets/loading2.json";
+import FolderOpenOutlinedIcon from "@mui/icons-material/FolderOpenOutlined";
 
 export default function HomePage() {
 	const [file, setFile] = useState(undefined);
@@ -16,6 +17,7 @@ export default function HomePage() {
 	};
 
 	const handleSubmit = async () => {
+		if (file === undefined) return;
 		setIsSearching(true);
 		const data = new FormData();
 		data.append("file", file);
@@ -26,6 +28,12 @@ export default function HomePage() {
 		setFileData(response.data);
 		setPage(0);
 		setIsSearching(false);
+	};
+
+	const resetFile = () => {
+		setFileData([]);
+		setPage(0);
+		setFile(undefined);
 	};
 
 	const toggleNextPage = () => {
@@ -47,127 +55,174 @@ export default function HomePage() {
 			rowGap={4}
 			minHeight="80vh"
 			maxWidth="100%"
-			sx={{ marginTop: "4rem", padding: "3vh" }}>
+			sx={{
+				marginTop: "4rem",
+				padding: "3vh",
+				paddingLeft: "13vh",
+				paddingRight: "13vh",
+			}}>
 			<Grid
 				container
 				style={{ overflow: "clip" }}
 				minHeight="40vh"
 				maxHeight="65vh">
 				<Grid item xs={12}>
-					<Box sx={{ boxShadow: 1, margin: "1vh" }}>
+					<Box sx={{ boxShadow: 1, margin: "1vh", height: "63vh" }}>
 						<Box
 							style={{
-								height: "42vh",
+								height: "41vh",
 								overflow: "scroll",
 							}}
 							sx={{
 								width: "96%",
 								padding: "3vh",
 							}}>
-							<Typography align="justify">
-								{isSearching ? (
-									<Lottie
-										loop
-										animationData={loadingJson}
-										play
-										style={{
-											width: 500,
-											height: 400,
-											marginLeft: "auto",
-											marginRight: "auto",
-										}}
+							{isSearching ? (
+								<Lottie
+									loop
+									animationData={loadingJson}
+									play
+									style={{
+										width: 500,
+										height: 400,
+										marginLeft: "auto",
+										marginRight: "auto",
+									}}
+								/>
+							) : fileData.length > 0 ? (
+								<Typography align="justify">{fileData[page]}</Typography>
+							) : (
+								<Box
+									sx={{ height: "100%" }}
+									display="flex"
+									justifyContent="center"
+									flexDirection="column"
+									alignItems="center">
+									<input
+										type="file"
+										id="icon-button-file"
+										hidden
+										onChange={handleChange}
 									/>
-								) : fileData.length > 0 ? (
-									fileData[page]
-								) : (
-									"Upload file to see results"
-								)}
-							</Typography>
+									<label htmlFor="icon-button-file">
+										<IconButton
+											color="#393e46"
+											component="span"
+											aria-label="add an alarm"
+											sx={{
+												mt: "6rem",
+												border: "1px dashed grey",
+												height: "10rem",
+												width: "10rem",
+											}}>
+											<FolderOpenOutlinedIcon sx={{ fontSize: "5rem" }} />
+										</IconButton>
+									</label>
+									<Typography variant="h6" color="text.secondary" m={2}>
+										Upload your file
+									</Typography>
+								</Box>
+							)}
 						</Box>
-						<Box
-							sx={{
-								padding: "1vh",
-							}}
-							display="flex"
-							justifyContent="center"
-							flexDirection="column"
-							alignItems="center"
-							columnGap={1}>
+						{fileData.length > 0 ? (
 							<Box
 								sx={{
-									padding: "3vh",
+									padding: "1vh",
 								}}
 								display="flex"
 								justifyContent="center"
-								flexDirection="row"
+								flexDirection="column"
 								alignItems="center"
 								columnGap={1}>
-								<Button
-									width="10%"
-									variant="contained"
+								<Box
 									sx={{
-										backgroundColor: "#393e46",
-										width: "7rem",
-										"&:hover": {
-											backgroundColor: "#393e46",
-											opacity: [0.9, 0.8, 0.7],
-										},
+										padding: "3vh",
 									}}
-									component="label"
-									onClick={togglePrevPage}>
-									Previous
-								</Button>
-								<Button
-									variant="contained"
-									sx={{
-										backgroundColor: "#393e46",
-										width: "7rem",
-										"&:hover": {
+									display="flex"
+									justifyContent="center"
+									flexDirection="row"
+									alignItems="center"
+									columnGap={1}>
+									<Button
+										width="10%"
+										variant="contained"
+										sx={{
 											backgroundColor: "#393e46",
-											opacity: [0.9, 0.8, 0.7],
-										},
-									}}
-									component="label"
-									onClick={toggleNextPage}>
-									Next
-								</Button>
+											width: "7rem",
+											"&:hover": {
+												backgroundColor: "#393e46",
+												opacity: [0.9, 0.8, 0.7],
+											},
+										}}
+										component="label"
+										onClick={togglePrevPage}>
+										Previous
+									</Button>
+									<Button
+										variant="contained"
+										sx={{
+											backgroundColor: "#393e46",
+											width: "7rem",
+											"&:hover": {
+												backgroundColor: "#393e46",
+												opacity: [0.9, 0.8, 0.7],
+											},
+										}}
+										component="label"
+										onClick={toggleNextPage}>
+										Next
+									</Button>
+								</Box>
+								{`Page ${page + 1} of ${fileData.length}`}
 							</Box>
-							{fileData.length > 0
-								? `Page ${page + 1} of ${fileData.length}`
-								: ""}
-						</Box>
+						) : (
+							""
+						)}
 					</Box>
 				</Grid>
 			</Grid>
-			<Button
-				variant="contained"
-				sx={{
-					backgroundColor: "#393e46",
-					width: "8rem",
-					"&:hover": {
-						backgroundColor: "#393e46",
-						opacity: [0.9, 0.8, 0.7],
-					},
-				}}
-				component="label">
-				Upload File
-				<input type="file" hidden onChange={handleChange} />
-			</Button>
-			<Button
-				variant="contained"
-				classes={"primary-button"}
-				sx={{
-					backgroundColor: "#f96d00",
-					width: "8rem",
-					"&:hover": {
+			<Box
+				display="flex"
+				justifyContent="end"
+				flexDirection="row-reverse"
+				alignItems="start"
+				columnGap={4}
+				sx={{ width: "100%" }}>
+				<Button
+					variant="contained"
+					classes={"primary-button"}
+					sx={{
 						backgroundColor: "#f96d00",
-						opacity: [0.9, 0.8, 0.7],
-					},
-				}}
-				component="label"
-				onClick={handleSubmit}>
-				Submit
-			</Button>
+						width: "12rem",
+						"&:hover": {
+							backgroundColor: "#f96d00",
+							opacity: [0.9, 0.8, 0.7],
+						},
+					}}
+					component="label"
+					onClick={handleSubmit}>
+					Submit
+				</Button>
+				{/* <Typography variant="caption" color="text.secondary" mt={1}>
+					or
+				</Typography> */}
+				<Button
+					variant="outlined"
+					onClick={resetFile}
+					sx={{
+						width: "12rem",
+						color: "#393e46",
+						borderColor: "#393e46",
+						"&:hover": {
+							opacity: [0.9, 0.8, 0.7],
+							backgroundColor: "white",
+							color: "#393e46",
+							borderColor: "#393e46",
+						},
+					}}>
+					Reset
+				</Button>
+			</Box>
 		</Box>
 	);
 }
